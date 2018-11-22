b696a4761ac226b20f5394c4aaf14aebeb68f003
ajajshaikh <ajajshaikh08@gmail.com>
2018-11-22 13:33:12 +0530

    updating

--- AjajShaikh_Oct2018_Part_1_1-Solution/src/SinglyLinkedList.java
+++ AjajShaikh_Oct2018_Part_1_1-Solution/src/SinglyLinkedList.java
@@ -1,16 +1,50 @@
-class SinglyLinkedList 
+import java.util.Stack;
+
+//import SinglyLinkedList.Node;
+
+public class SinglyLinkedList 
 { 
 	Node head;  // head of list 
 
-    static class Node { 
+    public class Node { 
         int data; 
         Node next; 
-        Node(int d)  { // Constructor
+        public Node(int d)  { // Constructor
         	data = d;  
         	next=null; 
         }  
     } 
     
-    
+    //Function to add new elements to the front of linklist
+	 public Node addfront(Node head,int val){
+	    	if(head==null){				//if linklist is empty then create linklist
+	    		head=new Node(val);
+	 	    }
+	    	else{
+	    		Node temp=new Node(val);
+	    		temp.next=head;
+	    		head=temp;
+	    	}
+	    	return head;
+	    }
+	 
+	 //function to update data till halfway of the list
+	 //new data of ith node = current data of ith node - data of (size-i)th node
+	 public Node updateList(Node head,int size) {
+		 Node temp=head;
+		 Stack<Integer> stack = new Stack<Integer>();	//Stack for storing the linklist in order to get elements in reverse order
+		 
+		 for(int i=0;i<size;i++){
+		 	stack.push(temp.data);		//pushing elements in stack
+		 	temp=temp.next;
+		 }
+		 temp=head;
+		 for(int i=0;i<size/2;i++){		//popping elements in reverse order
+		 	int val=stack.pop();
+		 	temp.data=temp.data - val;
+		 	temp=temp.next;
+		 }
+		 return head;
+	 }
    
 }
--- AjajShaikh_Oct2018_Part_1_1-Solution/src/Solution.java
+++ AjajShaikh_Oct2018_Part_1_1-Solution/src/Solution.java
@@ -12,7 +12,7 @@ public class Solution extends SinglyLinkedList {
 	 public static void main(String args[]){	
 		 int N,input,T;
 		 Scanner s=new Scanner(System.in);
-		 
+		 SinglyLinkedList l1 = new SinglyLinkedList();
 		 //Getting number of test cases as input from user
 		 do{
 			 T=s.nextInt();
@@ -31,12 +31,12 @@ public class Solution extends SinglyLinkedList {
 		 for(int i=0;i<N;i++){
 			 input=s.nextInt();
 			 if(i==0)
-				 head=new Node(input);
+				 head=l1.addfront(head, input);
 			 else
-				 head=addfront(head,input);		//function call for adding element to the front (head position)
+				 head=l1.addfront(head,input);		//function call for adding element to the front (head position)
 		 }
 		 
-		 head=updateData(head,N);		//function call to update data till halfway of list			
+		 head=l1.updateList(head,N);		//function call to update data till halfway of list			
 		 Node curr=head;
 		 System.out.println();			//printing the result
 		 while(curr!= null){
@@ -49,36 +49,6 @@ public class Solution extends SinglyLinkedList {
 		 s.close();
 	 }
 	 
-	 //Function to add new elements to the front of linklist
-	 static Node addfront(Node head,int val){
-	    	if(head==null){				//if linklist is empty then create linklist
-	    		head=new Node(val);
-	 	    }
-	    	else{
-	    		Node temp=new Node(val);
-	    		temp.next=head;
-	    		head=temp;
-	    	}
-	    	return head;
-	    }
-	 
-	 //function to update data till halfway of the list
-	 //new data of ith node = current data of ith node - data of (size-i)th node
-	 static Node updateData(Node head,int size) {
-		 Node temp=head;
-		 Stack<Integer> stack = new Stack<Integer>();	//Stack for storing the linklist in order to get elements in reverse order
-		 
-		 for(int i=0;i<size;i++){
-		 	stack.push(temp.data);		//pushing elements in stack
-		 	temp=temp.next;
-		 }
-		 temp=head;
-		 for(int i=0;i<size/2;i++){		//popping elements in reverse order
-		 	int val=stack.pop();
-		 	temp.data=temp.data - val;
-		 	temp=temp.next;
-		 }
-		 return head;
-	 }
+	
 	 
 }
